7bce5059b9c6e12fb7f6fc3f06673d0df24d1cd7
root <lanremidelola@gmail.com>
2023-12-20 22:38:13 +0000

    uploading files

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
-'''Task 10's module.
-'''
+""" MongoDB Operations with Python using pymongo """
 
-def update_topics(mongo_collection, name, topics):
-    '''
-    Update Topics in MongoDB Collection
-    
-    Parameters:
-        mongo_collection: MongoDB collection object
-        name: Name to identify the document to be updated
-        topics: List of topics to be set in the document
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection
+    based on keyword arguments.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection):
+        The MongoDB collection.
+        **kwargs: Keyword arguments representing the
+        fields and values for the new document.
 
     Returns:
-        None
-    '''
+        ObjectId: The ObjectId of the newly inserted document.
+    """
+    # Insert the document into the MongoDB collection
+    document_id = mongo_collection.insert(kwargs)
 
-    mongo_collection.update_many(
-        {'name': name},
-        {'$set': {'topics': topics}}
-    )
+    # Return the ObjectId of the newly inserted document
+    return document_id
